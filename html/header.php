ed125da292a3c6a6b3f9ff8a4187aa0ddce2bce5
BY Kim <logaegae@gmail.com>
2016-08-25 16:24:55 +0900

    1

---
+++ html/header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="mark">
+        <a href=""></a>
+    </div>
+    <div class="nav">
+       <button>Main Menu</button>
+       <ul>
+            <li><a href="">PROFILE</a></li>
+            <li><a href="">DISCOGRAPHY</a></li>
+            <li><a href="">GALLERY</a></li>
+            <li><a href="">VIDEO</a></li>
+            <li><a href="">NOTICE</a></li>
+            <li><a href="">SCHEDULE</a></li>
+       </ul>
+    </div>
+</header>
