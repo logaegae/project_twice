0d8e9a9fbe7ecda8bb50c575bae803d276916888
BY Kim <logaegae@gmail.com>
2016-08-31 18:27:26 +0000

    1

--- html/header.php
+++ html/header.php
@@ -1,16 +1,16 @@
 <header>
     <div class="mark">
-        <a href=""></a>
+        <a href="index.php"></a>
     </div>
     <div class="nav">
        <button>Main Menu</button>
        <ul>
-            <li><a href="">PROFILE</a></li>
-            <li><a href="">DISCOGRAPHY</a></li>
-            <li><a href="">GALLERY</a></li>
-            <li><a href="">VIDEO</a></li>
-            <li><a href="">NOTICE</a></li>
-            <li><a href="">SCHEDULE</a></li>
+            <li><a href="profile.php">PROFILE</a></li>
+            <li><a href="discography.php">DISCOGRAPHY</a></li>
+            <li><a href="gallery.php">GALLERY</a></li>
+            <li><a href="video.php">VIDEO</a></li>
+            <li><a href="notice.php">NOTICE</a></li>
+            <li><a href="schedule.php">SCHEDULE</a></li>
        </ul>
     </div>
 </header>
---
+++ html/schedule.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="웹 퍼블리셔 김범영의 포트폴리오 웹사이트입니다">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="BYK">
+    <title>Twice</title>
+    <!-- ===== Font ===== -->
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="../lib/css/flaticon.css" type="text/css" />
+    <!-- ===== style.css ===== -->
+    <link rel="stylesheet" href="../lib/css/style.css" type="text/css" />
+    <!-- ===== responsice.css ===== -->
+    <link rel="stylesheet" href="../lib/css/responsive.css" type="text/css" />
+    <!-- ===== JQuery ===== -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- ===== custom.js ===== -->
+    <script src="../lib/js/custom.js"></script>
+    
+    <style type="text/css">
+        /*bg*/
+        .schedule_wrapper
+        {
+            background-position: center top;
+            background-color: #f4f4f4;
+        }
+    </style>
+    <script type="text/javascript">
+    $(function(){
+        
+    });
+    </script>
+</head>
+    
+<body> 
+    <?php include("header.php"); ?>
+    <div class="schedule_wrapper">
+        <section>
+            
+            <div class="sect-title">NOTICE</div>
+            
+            <div class="sect-main">
+                
+                
+            
+            </div>
+        </section>
+    </div>
+    <?php include("footer.php"); ?>
+</body>
+</html>
+
